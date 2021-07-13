26206e73d37657d86e47de9e424ee1925bd27a2d
Tiah Keefe <tetered12@gmail.com>
2021-07-13 17:20:59 -0500

    Created a code that analizes a sentance and looks for a specific word then updates the user as to aspects of that word then deletes that word from the sentance.

--- stringPractice/Program.cs
+++ stringPractice/Program.cs
@@ -6,7 +6,26 @@ namespace stringPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string aliceInWonderlandSentance = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures or conversation?'";
+            Console.WriteLine("What word would you like to search?");
+            string searchedWord = Console.ReadLine();
+            bool wordFound = true;
+            if (aliceInWonderlandSentance.ToUpper().Contains(searchedWord.ToUpper()))
+                {
+                wordFound = true;
+                Console.WriteLine(wordFound);
+                int indexOfSearchedWord = aliceInWonderlandSentance.IndexOf(searchedWord);
+                Console.WriteLine(indexOfSearchedWord);
+                int lengthOfSearchedWord = searchedWord.Length;
+                Console.WriteLine(lengthOfSearchedWord);
+                string newSentance = aliceInWonderlandSentance.Remove(indexOfSearchedWord, lengthOfSearchedWord);
+                Console.WriteLine(newSentance);
+                } else
+                {
+                wordFound = false;
+                Console.WriteLine(wordFound);
+                }
+            
         }
     }
 }
